9405021876f60aa1c815e52beabcc5156afd62aa
waguiner89 <wagner.tech89@hotmail.com>
2023-12-14 13:06:33 -0300

    Envio de 2 exercicios python

---
+++ DesenvolvimentoWebCloud/Códigos/algoritmo_veiculos.py
@@ -0,0 +1,14 @@
+qtde_rodas = int(input("Digite a quantidade de rodas do veículo: "))
+peso = int(input("Digite o peso bruto em quilogramas: "))
+qtde_pessoas = int(input("Digite a quantidade máxima de pessoas no veículo: "))
+
+if qtde_rodas <= 3:
+    print("Categoria A")
+elif qtde_rodas == 4 and qtde_pessoas <= 8 and peso <= 3500:
+    print("Categoria B")
+elif qtde_rodas >= 4 and peso >= 3500 and peso <= 6000:
+    print("Categoria C")
+elif qtde_rodas >= 4 and qtde_pessoas > 8:
+    print("Categoria D")
+elif qtde_rodas >= 4 and peso > 6000:
+    print("Categoria E")
---
+++ DesenvolvimentoWebCloud/Códigos/andares_hotel.py
@@ -0,0 +1,4 @@
+for i in range(20, 0, -1):
+    if i == 13:
+        continue
+    print(i)
